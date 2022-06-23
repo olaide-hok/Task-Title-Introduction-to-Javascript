d54deaf87833996549298c5cb17d0be8030aaa7b
olaide-hok <olaidekareem43@yahoo.com>
2022-06-23 09:51:24 +0100

    Complete Task
    Create index.js which prints out your name, height and country on screen

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Task-Title-Introduction-to-Javascript
+A javascript code that prints out your name, height, and country on the screen.
---
+++ index.js
@@ -0,0 +1,8 @@
+// Assignment
+
+// Write a javascript code that prints out your name, height, and country on the screen
+const fname = 'Name: Habeeb Kareem'
+const height = 'Height: 6ft'
+const country = 'Country: Nigeria'
+
+console.log(fname, height, country)
